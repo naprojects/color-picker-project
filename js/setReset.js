55bcc7fdca41470d1b16c8f9ae7c46ba5f47e876
naprojects <naprojectsar@gmail.com>
2021-06-16 22:15:04 -0300

    [Cambios estÃeticos. Se cambia favicon y color de inicio/reset]

--- js/setReset.js
+++ js/setReset.js
@@ -1,6 +1,6 @@
 let setReset = () => {
-    color.value = '#ffffff'
-    salida.style.background = '#ffffff';
-    salida.style.color = '#000';
-    salida.innerHTML = '#ffffff';
+    color.value = '#8f1919'
+    salida.style.background = '#8f1919';
+    salida.style.color = '#fff';
+    salida.innerHTML = '#8f1919';
 };
--- js/variables.js
+++ js/variables.js
@@ -2,5 +2,5 @@ let color = document.querySelector('#color');
 let salida = document.querySelector('#salida');
 let iconoSalida = document.querySelector('#icono-salida');
 let reset = document.querySelector('#reset');
-let storage = localStorage.getItem('colorValue') || '#ffffff'; //Uso operador or porque si lo que trato de obtener es undefined o null, entonces aplico el blanco como valor predefinido.
+let storage = localStorage.getItem('colorValue') || '#8f1919'; //Uso operador or porque si lo que trato de obtener es undefined o null, entonces aplico un color como valor predefinido.
 let colorCopiado = [storage]; //Por defecto agrego storage para que cuando vuelva a ingresar pueda tomar el último valor que usé antes de cerrarlo.
